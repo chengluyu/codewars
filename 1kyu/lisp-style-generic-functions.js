edc708935a429b90d3e58103666b5af17097e471
chengluyu <chengluyu@live.cn>
2020-03-11 21:16:17 +0800

    Add 1kyu "Metaprogramming: Lisp-style Generic Functions" and test suites

---
+++ 1kyu/lisp-style-generic-functions.js
@@ -0,0 +1,270 @@
+// medium.com/fhinkel/v8-internals-how-small-is-a-small-integer-e0badc18b6da
+const V8_SMI_MAX = 1 << 31 - 1;
+const TYPE_MISMATCH = -1;
+
+class OverloadList {
+  constructor() {
+    this.overloads = [];
+    this.typesIndexMap = new Map();
+  }
+
+  add(overload) {
+    const index = this.typesIndexMap.get(overload.types);
+    if (index === undefined) {
+      this.typesIndexMap.set(overload.types, this.overloads.length);
+      this.overloads.push(overload);
+    } else {
+      this.overloads[index] = overload;
+    }
+  }
+
+  remove(overloadTypes) {
+    const index = this.typesIndexMap.get(overloadTypes);
+    if (index !== undefined) {
+      this.typesIndexMap.delete(overloadTypes);
+      this.overloads.splice(index, 1);
+    }
+  }
+
+  getCandidates(args, reverse = false) {
+    const factor = reverse ? -1 : 1
+    return this.overloads
+      .map(x => ({
+        overload: x,
+        specificity: x.guard.apply(null, args)
+      }))
+      .filter(x => x.specificity !== null)
+      .sort((a, b) => factor * compareSpecificity(a.specificity, b.specificity));
+  }
+}
+
+function getPrototypeChain(any) {
+  const chain = [];
+  let proto = any;
+  do {
+    proto = Object.getPrototypeOf(proto);
+    chain.push(proto.constructor.name);
+  } while (proto.constructor.name !== 'Object');
+  return chain;
+}
+
+function compareSpecificity(a, b) {
+  if (a.length !== b.length) {
+    throw new Error('specificity arrays must be of the same length');
+  }
+  const length = a.length;
+  for (let i = 0; i < length; i++) {
+    if (a[i] !== b[i]) {
+      return a[i] - b[i];
+    }
+  }
+  return 0;
+}
+
+function callNextMethod(context, ...args) {
+  if (context.primary) {
+    // The first time of execute the primary.
+    if (context.index === 0) {
+      context.index += 1;
+      // Before
+      for (const c of context.beforeCandidates) {
+        c.overload.implementation.apply(null, arguments);
+      }
+      // Primary
+      const ret = context.primaryCandidates[0].overload.implementation.apply(context, args);
+      // After
+      for (const c of context.afterCandidates) {
+        c.overload.implementation.apply(null, arguments);
+      }
+      return ret;
+    } else if (context.index < context.primaryCandidates.length) {
+      return context.primaryCandidates[context.index++].overload.implementation.apply(context, args);
+    } else {
+      throw new Error(`No next method found for ${context.name} in primary`);
+    }
+  } else if (context.index < context.aroundCandidates.length) {
+    const index = context.index;
+    // Advance the index.
+    context.index += 1;
+    // Switch to `primary` if the around candidates are run out.
+    if (context.index === context.aroundCandidates.length && context.primaryCandidates.length > 0) {
+      context.primary = true;
+      context.index = 0;
+    }
+    return context.aroundCandidates[index].overload.implementation.apply(context, arguments);
+  } else {
+    throw new Error(`No next method found for ${context.name} in around`);
+  }
+}
+
+function defgeneric(name) {
+  let version = 0;
+  const arounds = new OverloadList();
+  const befores = new OverloadList();
+  const overloads = new OverloadList();
+  const afters = new OverloadList();
+
+  function getTypeName(any) {
+    let type = typeof any;
+    if (type === 'object') {
+      if (any === null) {
+        type = 'null';
+      } else if (any.constructor.name !== 'Object') {
+        type = any.constructor.name;
+      }
+    }
+    return type;
+  }
+
+  function run(context, args) {
+    if (context.aroundCandidates.length > 0) {
+      context.primary = false;
+      return callNextMethod(context, ...args);
+    } else if (context.primaryCandidates.length > 0) {
+      context.primary = true;
+      return callNextMethod(context, ...args);
+    } else {
+      const argumentTypes = args.map(getTypeName).join(',');
+      throw new Error(`No method found for ${name} with args: ${argumentTypes}`)
+    }
+  }
+
+  function func(...args) {
+    return run({
+      name,
+      aroundCandidates: arounds.getCandidates(args),
+      primaryCandidates: overloads.getCandidates(args),
+      beforeCandidates: befores.getCandidates(args),
+      afterCandidates: afters.getCandidates(args, true),
+      primary: null,
+      index: 0,
+    }, args);
+  }
+
+  const findMethodCache = new Map()
+
+  function findMethod(...args) {
+    const aroundCandidates = arounds.getCandidates(args);
+    const primaryCandidates = overloads.getCandidates(args);
+    const beforeCandidates = befores.getCandidates(args);
+    const afterCandidates = afters.getCandidates(args, true);
+    const mostSpecificCandidate = (
+        aroundCandidates.length > 0
+      ? aroundCandidates[0]
+      : primaryCandidates.length > 0
+      ? primaryCandidates[0]
+      : null)
+    if (mostSpecificCandidate === null) {
+      const argumentTypes = [...arguments].map(getTypeName).join(',');
+      throw new Error(`No method found for ${name} with args: ${argumentTypes}`)
+    }
+
+    // Make a key from current version and types of the most specific candidate.
+    const key = `${version}_${mostSpecificCandidate.types}`;
+    let item = findMethodCache.get(key);
+
+    // If no cached function, make one.
+    if (item === undefined) {
+      item = (...args) => run({
+        name,
+        aroundCandidates,
+        primaryCandidates,
+        beforeCandidates,
+        afterCandidates,
+        primary: null,
+        index: 0,
+      }, args);
+
+      findMethodCache.set(key, item);
+    }
+
+    // Return the cached function.
+    return item;
+  }
+
+  /**
+   * Translate the argument type string to a guard function.
+   * @param {string} types a string describe types of arguments
+   * @returns {(...args: any[]) => number[] | null}
+   */
+  function parseGuard(types) {
+    const guards = types.split(/\s*,\s*/g).map(type => {
+      switch (type) {
+        case '*':
+          return () => V8_SMI_MAX;
+        case 'undefined':
+          return x => x === undefined ? 0 : TYPE_MISMATCH;
+        case 'null':
+          return x => x === null ? 0 : TYPE_MISMATCH;
+        case 'boolean':
+          return x => typeof x === 'boolean' ? 0 : TYPE_MISMATCH;
+        case 'number':
+          return x => typeof x === 'number' ? 0 : TYPE_MISMATCH;
+        case 'string':
+          return x => typeof x === 'string' ? 0 : TYPE_MISMATCH;
+        case 'object':
+          return x => typeof x === 'object' && x !== null ? 0 : TYPE_MISMATCH;
+        case 'function':
+          return x => typeof x === 'function' ? 0 : TYPE_MISMATCH;
+        default:
+          return x => getPrototypeChain(x).indexOf(type);
+      }
+    })
+    return function () {
+      if (arguments.length !== guards.length) {
+        return null;
+      }
+      const specificity = guards.map((g, i) => g(arguments[i]));
+      return specificity.indexOf(TYPE_MISMATCH) >= 0 ? null : specificity;
+    }
+  }
+
+  func.defmethod = function (types, implementation, combination = 'primary') {
+    version += 1;
+    const overload = {
+      types,
+      guard: parseGuard(types),
+      implementation,
+      combination,
+    };
+    if (combination === 'before') {
+      befores.add(overload);
+    } else if (combination === 'primary') {
+      overloads.add(overload);
+    } else if (combination === 'after') {
+      afters.add(overload);
+    } else if (combination === 'around') {
+      arounds.add(overload);
+    } else {
+      throw new Error(`unknown combination "${combination}"`);
+    }
+    return func;
+  }
+
+  func.removeMethod = function (types, combination = 'primary') {
+    version += 1;
+    if (combination === 'before') {
+      befores.remove(types);
+    } else if (combination === 'primary') {
+      overloads.remove(types);
+    } else if (combination === 'after') {
+      afters.remove(types);
+    } else if (combination === 'around') {
+      arounds.remove(types);
+    } else {
+      throw new Error(`unknown combination "${combination}"`);
+    }
+    return func;
+  }
+
+  func.findMethod = findMethod;
+
+  return func;
+}
+
+module.exports = {
+  getPrototypeChain,
+  compareSpecificity,
+  callNextMethod,
+  defgeneric,
+}
---
+++ 1kyu/lisp-style-generic-functions.test.js
@@ -0,0 +1,179 @@
+const {
+  getPrototypeChain,
+  defgeneric,
+  callNextMethod,
+  compareSpecificity
+} = require('./lisp-style-generic-functions');
+
+class Mammal {}
+class Platypus extends Mammal {}
+class Rhino extends Mammal {}
+
+describe('getPrototypeChain', () => {
+  it('Intrinsic types', () => {
+    expect(getPrototypeChain(0)).toEqual(['Number', 'Object']);
+    expect(getPrototypeChain('test')).toEqual(['String', 'Object']);
+    expect(getPrototypeChain(true)).toEqual(['Boolean', 'Object']);
+    expect(getPrototypeChain(function () {})).toEqual(['Function', 'Object']);
+  })
+  it('User-defined classes', () => {
+    expect(getPrototypeChain(new Mammal())).toEqual(['Mammal', 'Object']);
+    expect(getPrototypeChain(new Rhino())).toEqual(['Rhino', 'Mammal', 'Object']);
+  })
+})
+
+describe('compareSpecificity', () => {
+  it('empty arrays', () => {
+    expect(compareSpecificity([], [])).toEqual(0)
+  })
+  it('same arrays', () => {
+    expect(compareSpecificity([0], [0])).toEqual(0)
+    expect(compareSpecificity([0, 8], [0, 8])).toEqual(0)
+    expect(compareSpecificity([0, 8, 16], [0, 8, 16])).toEqual(0)
+  })
+  it('different arrays', () => {
+    expect(compareSpecificity([0], [1])).toEqual(-1)
+    expect(compareSpecificity([0, 8], [0, 7])).toEqual(1)
+    expect(compareSpecificity([0, 8, 16], [0, 0, 16])).toEqual(8)
+  })
+  it('sort', () => {
+    const a = [1, 2, 3]
+    const b = [1, 1, 0]
+    const c = [2, 3, 4]
+    const d = [0, 8, 2]
+    expect([a, b, c, d].sort(compareSpecificity)).toEqual([d, b, a, c])
+  })
+})
+
+describe('defgeneric', () => {
+  it('example 1', () => {
+    var append = defgeneric('append');
+    append.defmethod('Array,Array', function (a,b) { return a.concat(b); });
+    append.defmethod('*,Array', function (a,b) { return [a].concat(b); });
+    append.defmethod('Array,*', function (a,b) { return a.concat([b]); });
+
+    expect(append([1,2],[3,4])).toEqual([1,2,3,4]);
+    expect(append(1,[2,3,4])).toEqual([1,2,3,4]);
+    expect(append([1,2,3],4)).toEqual([1,2,3,4]);
+    expect(append.bind(null, 1, 2, 3, 4)).toThrow(
+      'No method found for append with args: number,number,number,number'
+    );
+  })
+
+  it('example 2', () => {
+    var laysEggs = defgeneric('laysEggs')
+      .defmethod('Mammal', function () { return false; })
+      .defmethod('Platypus', function () { return true; });
+    expect(laysEggs(new Rhino())).toBe(false);
+    expect(laysEggs(new Platypus())).toBe(true);
+    expect(laysEggs.bind(null, 5)).toThrow('No method found for laysEggs with args: number');
+  })
+
+  it('before & after', () => {
+    let lines = [];
+    var laysEggs = defgeneric('laysEggs')
+      .defmethod('Mammal', function () { return false; })
+      .defmethod('Platypus', function () { return true; })
+      .defmethod('Platypus', function () { lines.push('Before platypus egg check.'); }, 'before')
+      .defmethod('Mammal', function () { lines.push('Before mammal egg check.'); }, 'before')
+      .defmethod('*', function () { lines.push('Before egg check.'); }, 'before')
+      .defmethod('Platypus', function () { lines.push ('After platypus egg check.'); }, 'after')
+      .defmethod('Mammal', function () { lines.push('After mammal egg check.'); }, 'after');
+
+    expect(laysEggs(new Platypus())).toEqual(true);
+    expect(lines).toEqual([
+      'Before platypus egg check.',
+      'Before mammal egg check.',
+      'Before egg check.',
+      'After mammal egg check.',
+      'After platypus egg check.',
+    ]);
+  })
+
+  it('callNextMethod', () => {
+    var describe = defgeneric('describe')
+      .defmethod('Mammal', function () { return "Warm-blooded animal with large four-chambered heart."; })
+      .defmethod('Platypus', function (p) { return callNextMethod(this,p) + " [Aquatic]"; });
+    expect(describe(new Platypus())).toEqual(
+      'Warm-blooded animal with large four-chambered heart. [Aquatic]'
+    );
+  });
+
+  it('callNextMethod & around', () => {
+    const logs = [];
+    var laysEggs = defgeneric('laysEggs')
+      .defmethod('Mammal', function () { return false; })
+      .defmethod('Platypus', function () { return true; });
+    laysEggs
+      .defmethod('Platypus', function () { logs.push('Before platypus egg check.'); }, 'before')
+      .defmethod('Mammal', function () { logs.push('Before mammal egg check.'); }, 'before')
+      .defmethod('*', function () { logs.push('Before egg check.'); }, 'before')
+      .defmethod('Platypus', function () { logs.push ('After platypus egg check.'); }, 'after')
+      .defmethod('Mammal', function () { logs.push('After mammal egg check.'); }, 'after');
+    laysEggs.defmethod('Platypus', function (p) {
+      logs.push('>>>Around platypus check.');
+      var ret = callNextMethod(this,p);
+      logs.push('<<<Around platypus check.');
+      return ret;
+    }, 'around');
+    laysEggs.defmethod('Mammal', function (p) {
+      logs.push('>>>Around mammal check.');
+      var ret = callNextMethod(this, p);
+      logs.push('<<<Around mammal check.');
+      return ret;
+    }, 'around');
+    expect(laysEggs(new Platypus())).toBe(true);
+    expect(logs).toEqual([
+      '>>>Around platypus check.',
+      '>>>Around mammal check.',
+      'Before platypus egg check.',
+      'Before mammal egg check.',
+      'Before egg check.',
+      'After mammal egg check.',
+      'After platypus egg check.',
+      '<<<Around mammal check.',
+      '<<<Around platypus check.',
+    ]);
+  });
+
+  it('findMethod', () => {
+    var append = defgeneric('append');
+    append.defmethod('Array,Array', function (a,b) { return a.concat(b); });
+    append.defmethod('*,Array', function (a,b) { return [a].concat(b); });
+    append.defmethod('Array,*', function (a,b) { return a.concat([b]); });
+
+    var appendLists = append.findMethod([], []);
+    expect(appendLists([1,2], [3,4])).toEqual(append([1,2],[3,4]));
+  })
+
+  it('findMethod with same type will return the same function', () => {
+    var describe = defgeneric('describe')
+      .defmethod('Mammal', function () { return "Warm-blooded animal with large four-chambered heart."; })
+      .defmethod('Platypus', function (p) { return callNextMethod(this,p) + " [Aquatic]"; });
+
+    var find1 = describe.findMethod(new Platypus());
+    var find2 = describe.findMethod(new Platypus());
+
+    describe.removeMethod('Platypus');
+
+    var find3 = describe.findMethod(new Platypus());
+    var find4 = describe.findMethod(new Mammal());
+
+    expect(find1).toBe(find2);
+    expect(find3).toBe(find4);
+  })
+
+  it('name', () => {
+    var name = defgeneric('name')
+      .defmethod('Mammal', function () { return 'Mammy'; })
+      .defmethod('Platypus', function (p) { return 'Platy ' + callNextMethod(this,p); });
+
+    var find1 = name.findMethod(new Platypus());
+
+    name.defmethod('Platypus', function () { return 'Pat'; });
+    name.removeMethod('Mammal');
+
+    expect(find1(new Platypus())).toBe('Platy Mammy');
+    expect(name(new Platypus())).toBe('Pat');
+  })
+})
